2d7ec1a431536b0ef143a9aff1e26f0196881db4
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-31 19:36:48 -0400

    Cambios para React 18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById('App'));
+const root = ReactDOM.createRoot(document.getElementById("App"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
